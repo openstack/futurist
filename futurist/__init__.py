17d7be69fae275993e88f995bca7aa8a9c1e75ce
Joshua Harlow <harlowja@yahoo-inc.com>
2015-06-01 16:14:09 -0700

    Rename futures.py -> _futures.py and expose at futurist module
    Instead of having to expose the underlying futures impl and the
    code and modules associated just expose at the futurist module level
    the classes we want to expose (to avoid having to expose any inner
    module details that people should not be using).
    
    Change-Id: Ib53052a2df5c673b6e0f6956398b56a4f99f93c5

---
+++ futurist/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#    Copyright (C) 2015 Yahoo! Inc. All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Promote accessible items to this module namespace (for easy access).
+
+from futurist._futures import Future  # noqa
+from futurist._futures import GreenFuture  # noqa
+
+from futurist._futures import GreenThreadPoolExecutor  # noqa
+from futurist._futures import ProcessPoolExecutor  # noqa
+from futurist._futures import SynchronousExecutor  # noqa
+from futurist._futures import ThreadPoolExecutor  # noqa
+
+from futurist._futures import ExecutorStatistics  # noqa
